cb0c176f519646aad12d6a3a7fdc14b3874c7715
dan jones <dannyjones360@gmail.com>
2017-05-16 19:17:28 +0100

    Added default dubRage stats

--- index.js
+++ index.js
@@ -1,68 +1,85 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  //---- GLOBALS ----
-  var ROUND = 1;
-  var P1;
-  var P1_TURN = 1;
-  var P2;
-  var P2_TURN = 0;
+    //---- GLOBALS ----
+    var ROUND = 1;
 
-  var characters = {
-    Simula: {
-      name: "Simula",
-      health: 100,
-      defaultAttack: 18,
-      attacks: {
-        special1: 15
-      }
-    },
-    Noisia: {
-      name: "Noisia",
-      health: 300,
-      defaultAttack: 12,
-      attacks: {
-        special1: 20
-      }
-    }
-  };
+    var P1;
+    var P1_TURN = 1;
 
-  P1 = characters.Simula;
-  P2 = characters.Noisia;
+    var P2;
+    var P2_TURN = 0;
 
-  document.getElementById("attack").onclick = function() { //asign a function
-    console.log(P1);
-    console.log(P2);
-  }
+    var characters = {
+        Simula: {
+            name: "Simula",
+            health: 100,
+            dubRage: 20,
+            defaultAttack: 18,
+            attacks: {
+                special_1: {
+                    name: "Nasssty frog synths",
+                    attack: 25,
+                    mannaRequired: 100
+                }
+            }
+        },
+        Noisia: {
+            name: "Noisia",
+            health: 300,
+            defaultAttack: 12,
+            dubRage: 50,
+            special_1: {
+                name: "Diplodocus",
+                attack: 35,
+                mannaRequired: 140
+            }
+        }
+    };
 
-  document.getElementById("defaultAttack").onclick = function() { //asign a function
-    if (P1_TURN == 1) {
-      playMove(P1, P2);
-    } else {
-      playMove(P2, P1);
-      ROUND++
-    }
-  }
+    P1 = characters.Simula;
+    P2 = characters.Noisia;
+    document.getElementById("p1_name").innerHTML = P1.name
+    document.getElementById("p2_name").innerHTML = P2.name
 
-  //plays the move
-  function playMove(attacker, defender) {
-    var damage = attacker.defaultAttack;
-    defender.health -= damage;
 
-    console.log("Round: " + ROUND)
-    console.log(attacker.name + " does " + damage + " damage to " + defender.name);
-    console.log(defender.name + " health is now " + defender.health);
-    console.log("Turn over");
 
-    if (defender.health <= 0) {
-      console.log(defender.name + " is dead, game over... " + attacker.name + " won!");
+    document.getElementById("attack").onclick = function() {
+      console.log(P1);
+      console.log(P2);
     }
 
-    if (P1_TURN == 1) {
-      P1_TURN = 0;
-      P2_TURN = 1;
-    } else {
-      P1_TURN = 1;
-      P2_TURN = 0;
+
+    document.getElementById("defaultAttack").onclick = function() {
+        if (P1_TURN == 1) {
+            playMove(P1, P2);
+        } else {
+            playMove(P2, P1);
+            P1.dubRage += 20;
+            P2.dubRage += 20;
+            ROUND++;
+        }
     }
-  }
+
+    //plays the move
+    function playMove(attacker, defender) {
+        var damage = attacker.defaultAttack;
+        defender.health -= damage;
+
+        console.log("Round: " + ROUND)
+        console.log(attacker.name + " does " + damage + " damage to " + defender.name);
+        console.log(defender.name + " health is now " + defender.health);
+        console.log("Turn over");
+
+        if (defender.health <= 0) {
+            console.log(defender.name + " is dead, game over... " + attacker.name + " won!");
+        }
+
+        if (P1_TURN == 1) {
+            P1_TURN = 0;
+            P2_TURN = 1;
+        } else {
+            P1_TURN = 1;
+            P2_TURN = 0;
+        }
+    }
 });
